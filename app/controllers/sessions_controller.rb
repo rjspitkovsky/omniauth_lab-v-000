33f6345daab14b62af44e0fd4ff321051431d092
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-22 21:23:23 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
 
   def create
   if  auth_hash = request.env['omniauth.auth']
-    @user = User.find_or_create_by_omniauth(auth_hash)
-    session[:user_id] = @user.id
+    user = User.find_or_create_by_omniauth(auth_hash)
+    session[:user_id] = user.id
     redirect_to root_path
 
   else
