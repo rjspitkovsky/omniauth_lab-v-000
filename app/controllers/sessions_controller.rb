d9bf473be46883f968bbc8570f45cb18d1abc0db
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-22 21:17:02 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     redirect_to root_path
 
   else
-    user = User.find_by(:email => params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(:email => params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
