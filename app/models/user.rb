bb5ef1f10f311ceb18092eb6c54a3d605349e637
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-22 21:27:15 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
   #has_secure_password
 
   def self.find_or_create_by_omniauth(auth_hash)
-    self.where(:uid => auth_hash[:info][:uid]).first_or_create do |user|
+    self.where(:email => auth_hash[:info][:email]).first_or_create do |user|
       user.name = auth_hash[:info][:name]
-      user.email = auth_hash[:info][:email]
+      user.uid = auth_hash[:info][:uid]
       user.image = auth_hash[:info][:image]
     end
   end
