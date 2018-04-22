51ebc3e2d25afd16687b917885451a6aeb49fb08
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-22 21:16:47 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   if  auth_hash = request.env['omniauth.auth']
     user = User.find_or_create_by_omniauth(auth_hash)
     session[:user_id] = user.id
+    redirect_to root_path
 
   else
     user = User.find_by(:email => params[:email])
