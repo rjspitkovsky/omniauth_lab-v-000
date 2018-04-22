1651a69d66e4163e3f26d220bf6a0a1ce80086fd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-22 21:36:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
    if  auth_hash = request.env['omniauth.auth']
 
-     @user = User.find_or_create_by(uid: auth['uid']) do |u|
+     @user = User.find_or_create_by(uid: auth_hash['uid']) do |u|
 			u.name = auth['info']['name']
 			u.email = auth['info']['email']
 			u.image = auth['info']['image']
