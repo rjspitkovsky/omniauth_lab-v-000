db1984dab9381126f6b13ec81a84cb34df0faa90
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-22 21:33:48 +0000

    automatically backed up by learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['138551276864244'], ENV['64754899e753de6555b95e783919e842']
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
 
 
-FACEBOOK_KEY = '138551276864244'
-FACEBOOK_SECRET = '64754899e753de6555b95e783919e842'
+
